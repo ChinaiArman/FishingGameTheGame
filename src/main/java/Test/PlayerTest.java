0950410921deaa3fffbe70ccc058ecc6673ecd09
cdoig4 <cdoig4@my.bcit.ca>
2023-04-06 13:13:38 -0700

    Update tests to remove run issues

--- src/main/java/Test/PlayerTest.java
+++ src/main/java/Test/PlayerTest.java
@@ -4,101 +4,66 @@ import org.junit.Test;
 import org.junit.Assert;
 
 public class PlayerTest {
-    @Test
-    public void testGetRodLevel() {
-        Assert.assertEquals(0, Player.getRodLevel());
-    }
 
     @Test
     public void testIncrementRodLevel() {
+        Assert.assertEquals(0, Player.getRodLevel());
         Player.incrementRodLevel();
         Assert.assertEquals(1, Player.getRodLevel());
     }
 
     @Test
-    public void testGetBaitLevel() {
-        Assert.assertEquals(0, Player.getBaitLevel());
-    }
-
-    @Test
     public void testIncrementBaitLevel() {
+        Assert.assertEquals(0, Player.getBaitLevel());
         Player.incrementBaitLevel();
         Assert.assertEquals(1, Player.getBaitLevel());
     }
 
     @Test
-    public void testGetFishCount() {
-        Assert.assertEquals(0, Player.getFishCount());
-    }
-
-    @Test
     public void testSetFishCount() {
+        Assert.assertEquals(0, Player.getFishCount());
         Player.setFishCount(10);
         Assert.assertEquals(10, Player.getFishCount());
     }
 
     @Test
-    public void testGetCoinCount() {
-        Assert.assertEquals(0, Player.getCoinCount());
-    }
-
-    @Test
     public void testSetCoinCount() {
+        Assert.assertEquals(0, Player.getCoinCount());
         Player.setCoinCount(5);
         Assert.assertEquals(5, Player.getCoinCount());
     }
 
     @Test
-    public void testGetCurrentScore() {
-        Assert.assertEquals(0, Player.getCurrentScore());
-    }
-
-    @Test
     public void testSetCurrentScore() {
+        Assert.assertEquals(0, Player.getCurrentScore());
         Player.setCurrentScore(40);
         Assert.assertEquals(40, Player.getCurrentScore());
     }
 
     @Test
-    public void testIsOceanUnlocked() {
-        Assert.assertFalse(Player.isOceanUnlocked());
-    }
-
-    @Test
     public void testUnlockOcean() {
+        Assert.assertFalse(Player.isOceanUnlocked());
         Player.unlockOcean();
         Assert.assertTrue(Player.isOceanUnlocked());
     }
 
     @Test
-    public void testGetLastScene() {
-        Assert.assertNull(Player.getLastScene());
-    }
-
-    @Test
     public void testSetLastScene() {
+        Assert.assertNull(Player.getLastScene());
         Player.setLastScene("Controller.fxml");
         Assert.assertEquals("Controller.fxml", Player.getLastScene());
     }
 
     @Test
-    public void testGetCatchThreshold() {
-        Assert.assertEquals(120, Player.getCatchThreshold());
-    }
-
-    @Test
     public void testDecrementThreshold() {
+        Assert.assertEquals(120, Player.getCatchThreshold());
         Player.decrementThreshold();
         Assert.assertEquals(115, Player.getCatchThreshold());
     }
 
     @Test
-    public void testGetMaxBaitStrength() {
-        Assert.assertEquals(150, Player.getMaxBaitStrength());
-    }
-
-    @Test
     public void testIncreaseBaitStrength() {
+        Assert.assertEquals(150, Player.getMaxBaitStrength());
         Player.increaseBaitStrength();
         Assert.assertEquals(140, Player.getMaxBaitStrength());
     }
